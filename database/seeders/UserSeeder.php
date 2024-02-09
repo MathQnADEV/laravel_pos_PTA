f392125810999a0192d394cc68ced1cd15fef015
MathQnADEV <mathqna.project@gmail.com>
2024-02-09 18:21:56 +0800

    fix: user & product

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            "name" => "shaq",
+            "email" => "shaqt9center@gmail.com",
+            "password" => Hash::make("1234567890"),
+            "roles" => "ADMIN",
+            "phone" => "081234567890",
+        ]);
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{ route('home') }}">POS CUY</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{ route('home') }}">PC</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ route('home') }}">General Dashboard</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-user"></i><span>Users</span></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="{{ route('user.index') }}">All Users</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i
+                        class="fas fa-cubes-stacked"></i><span>Products</span></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="{{ route('product.index') }}">All Products</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
